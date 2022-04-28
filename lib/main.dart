cd94dd0dae77558fb1b4c0725b9ef4b7d0f4d004
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-04-28 15:00:38 +0800

    finish

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:game_store/screens/home/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent)
+    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Game Store',
+      theme: ThemeData(
+      
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
---
+++ lib/screens/detail/game_info.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:game_store/models/game.dart';
+import 'package:game_store/screens/detail/game_desc.dart';
+import 'package:game_store/screens/detail/game_gallery.dart';
+import 'package:game_store/screens/detail/game_review.dart';
+
+import 'game_header.dart';
+
+class GameInfo extends StatelessWidget {
+  final Game game;
+  const GameInfo(this.game, { Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(children: [
+        GameHeader(game),
+        GameGallery(game),
+        GameDesc(game),
+        GameReview(game)
+        
+      ],),
+    );
+  }
+}
